58c5a00c54c5baeadd3a46f3b77e5cca41139c1a
bryancitu <bryanjesus.ra@gmail.com>
2021-07-01 18:40:09 -0500

    compras detail page 8.0

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import BtnDarkMode from '../components/BtnDarkMode'
-import Logo2 from '../assets/img/logo-synho-2.jpeg'
 import '../assets/css/Header.css'
 
 const Header = () => {
     return (
         <header>
-            <div className="logo-header"><img src={Logo2} alt="synho" style={{height:"55px"}}/></div>
+            <div className="logo-header">SYNHO</div>
             <div>
                 <BtnDarkMode/>
             </div>
